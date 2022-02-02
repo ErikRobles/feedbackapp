a5c12de4102cb0823d646c5e133b72247a8abf80
ErikRobles <erikjames69@hotmail.com>
2022-02-02 12:41:52 -0600

    changed DBs

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,11 @@
 import { createContext, useState, useEffect } from 'react';
+import easyDB from 'easydb-io';
+import { v4 as uuidv4 } from 'uuid';
+
+const db = easyDB({
+  database: process.env.REACT_APP_EASYDB_DB,
+  token: process.env.REACT_APP_EASYDB_TOKEN,
+});
 
 const FeedbackContext = createContext();
 
@@ -16,72 +23,78 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const devEnv = process.env.NODE_ENV !== 'production';
-    const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+    // const devEnv = process.env.NODE_ENV !== 'production';
+    // const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
     // const response = await fetch(`/feedback?_sort=id&_order=desc`);
-    const response = await fetch(
-      `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}?_sort=id&_order=desc`
-    );
+    // const response = await fetch(
+    //   `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}?_sort=id&_order=desc`
+    // );
 
-    const data = await response.json();
+    const data = await db.list();
 
-    setFeedback(data);
+    setFeedback(Object.values.data);
     setIsLoading(false);
   };
 
   // Add feedback
   const addFeedback = async (newFeedback) => {
-    const devEnv = process.env.NODE_ENV !== 'production';
-    const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
-    const response = await fetch(
-      `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newFeedback),
-      }
-    );
+    // const devEnv = process.env.NODE_ENV !== 'production';
+    // const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+    // const response = await fetch(
+    //   `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}`,
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(newFeedback),
+    //   }
+    // );
 
-    const data = await response.json();
+    // const data = await response.json();
 
-    setFeedback([data, ...feedback]);
+    const id = uuidv4();
+    const feedbackToAdd = { ...newFeedback, id };
+    await db.put(id, feedbackToAdd);
+    setFeedback([feedback, ...feedbackToAdd]);
   };
 
   // Delete feedback
   const deleteFeedback = async (id) => {
-    const devEnv = process.env.NODE_ENV !== 'production';
-    const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+    // const devEnv = process.env.NODE_ENV !== 'production';
+    // const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
     if (window.confirm('Are you sure you want to delete?')) {
-      await fetch(`${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/${id}`, {
-        method: 'DELETE',
-      });
-
+      await db.delete(id);
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update feedback item
   const updateFeedback = async (id, updItem) => {
-    const devEnv = process.env.NODE_ENV !== 'production';
-    const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
-    const response = await fetch(
-      `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/${id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updItem),
-      }
-    );
+    // const devEnv = process.env.NODE_ENV !== 'production';
+    // const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+    // const response = await fetch(
+    //   `${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/${id}`,
+    //   {
+    //     method: 'PUT',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(updItem),
+    //   }
+    // );
+    const updatedFeedback = { ...updItem, id };
+    await db.put(id, updatedFeedback);
 
     const data = await response.json();
 
     setFeedback(
       feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    });
   };
 
   // Set item to be updated
